7f69cc9c12dfa833264c34a43ff0a1c3e201a8ee
Natalya Derkach <berterstm@gmail.com>
2019-10-01 14:51:41 +0300

    init

---
+++ producer/api.go
@@ -0,0 +1,14 @@
+package producer
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
